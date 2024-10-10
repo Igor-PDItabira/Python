be675bdd72abed25b6d2b57321503579b24f82fc
Igor-PDItabira <163460892+Igor-PDItabira@users.noreply.github.com>
2024-10-10 16:37:15 -0300

    Add files via upload

---
+++ Modulo 2/exercicio 2.2.py
@@ -0,0 +1,55 @@
+print('----------- Questão 1 ---------------')
+
+val1 = 5
+val2 = 2
+print(type(val1))
+print(type(val2))
+
+divisao = val1 / val2
+print(divisao)
+print(type(divisao))
+
+
+print('----------- Questão 2 ---------------')
+
+var1 = 'o resultado é'
+var2 = 10
+var3 = 3.5
+
+soma = var2 + var3
+
+print(var1)
+print(type(var1))
+print(var2)
+print(type(var2))
+print(var3)
+print(type(var3))
+print(soma)
+print(type(soma))
+
+
+print('----------- Questão 3 ---------------')
+
+v1 = 10
+v2 = 20
+print(v1)
+print(v2)
+
+aux = v1
+v1 = v2
+v2 = aux
+
+print(v1)
+print(v2)
+
+
+print('----------- Questão 4 ---------------')
+
+juros = 1.01
+saldo = 500.0
+
+saldo = saldo * juros
+saldo = saldo * juros
+saldo = saldo * juros
+print("Após 3 meses meu novo saldo é")
+print(saldo)
